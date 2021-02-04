a559d64b9732c4b0257da72bf280f795a2085f66
ibqn <evgen.ibqn@gmail.com>
2021-02-05 00:45:20 +0100

    add border around raitng

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,23 @@
 import { Fragment } from 'react'
+import styled from 'styled-components'
 import GlobalStyles from './components/global-styles'
 import StarRating from './components/star-rating'
+
+const StyledDiv = styled.div`
+  background-color: #fff;
+  border-radius: 6px;
+  box-shadow: 0 2px 3px rgb(10 10 10 / 10%), 0 0 0 1px rgb(10 10 10 / 10%);
+  color: #4a4a4a;
+  padding: 1.25rem;
+`
 
 const App = () => {
   return (
     <Fragment>
-      <div>Star rating</div>
-      <StarRating />
+      <StyledDiv>
+        <div>Star rating</div>
+        <StarRating />
+      </StyledDiv>
       <GlobalStyles />
     </Fragment>
   )
